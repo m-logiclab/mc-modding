1821d2834ad9addb128790920bb8eefa4218b487
m-logiclab <mio362@hotmail.com>
2022-02-20 01:34:51 +0900

    第4回：ツール追加

--- src/main/java/com/example/examplemod/world/item/ModItems.java
+++ src/main/java/com/example/examplemod/world/item/ModItems.java
@@ -2,9 +2,7 @@ package com.example.examplemod.world.item;
 
 import com.example.examplemod.Sample01;
 import com.example.examplemod.world.level.block.ModBlocks;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.Item;
+import net.minecraft.world.item.*;
 import net.minecraftforge.fmllegacy.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -16,8 +14,34 @@ public class ModItems {
 
     public static final RegistryObject<Item> PINK_DIAMOND =
             ITEMS.register( "pink_diamond" , () -> new Item( new Item.Properties().tab(CreativeModeTab.TAB_MISC) ) );
+
     public static final RegistryObject<Item> PINK_DIAMOND_ORE =
             ITEMS.register( "pink_diamond_ore" , ()-> new BlockItem(ModBlocks.PINK_DIAMOND_ORE.get(),
                     new Item.Properties().tab(CreativeModeTab.TAB_BUILDING_BLOCKS)));
+
+    public static final RegistryObject<Item> PINK_DIAMOND_PICKAXE =
+            ITEMS.register("pink_diamond_pickaxe",
+                    () -> new PickaxeItem( ModTiers.PINK_DIAMOND , 1 , -2.8F ,
+                            new Item.Properties().tab(CreativeModeTab.TAB_TOOLS)));
+
+    public static final RegistryObject<Item> PINK_DIAMOND_AXE =
+            ITEMS.register("pink_diamond_axe",
+                    () -> new AxeItem( ModTiers.PINK_DIAMOND , 5.0F, -3.0F ,
+                            new Item.Properties().tab(CreativeModeTab.TAB_TOOLS)));
+
+    public static final RegistryObject<Item> PINK_DIAMOND_HOE =
+            ITEMS.register("pink_diamond_hoe",
+                    () -> new HoeItem( ModTiers.PINK_DIAMOND , -3, 0.0F ,
+                            new Item.Properties().tab(CreativeModeTab.TAB_TOOLS)));
+
+    public static final RegistryObject<Item> PINK_DIAMOND_SHOVEL =
+            ITEMS.register("pink_diamond_shovel",
+                    () -> new ShovelItem( ModTiers.PINK_DIAMOND , 1.5F, -3.0F ,
+                            new Item.Properties().tab(CreativeModeTab.TAB_TOOLS)));
+
+    public static final RegistryObject<Item> PINK_DIAMOND_SWORD =
+            ITEMS.register("pink_diamond_sword",
+                    () -> new SwordItem( ModTiers.PINK_DIAMOND , 3, -2.4F ,
+                            new Item.Properties().tab(CreativeModeTab.TAB_COMBAT)));
 
 }
---
+++ src/main/java/com/example/examplemod/world/item/ModTiers.java
@@ -0,0 +1,64 @@
+package com.example.examplemod.world.item;
+
+import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.util.LazyLoadedValue;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.Tier;
+import net.minecraft.world.item.crafting.Ingredient;
+
+import java.util.function.Supplier;
+
+public enum ModTiers implements Tier {
+
+    PINK_DIAMOND(3, 3000, 24.0F, 9.0F, 22, () -> {
+        return Ingredient.of(ModItems.PINK_DIAMOND.get());
+
+    });
+
+    private final int level;
+    private final int uses;
+    private final float speed;
+    private final float damage;
+    private final int enchantmentValue;
+    private final LazyLoadedValue<Ingredient> repairIngredient;
+
+    private ModTiers(int p_43332_, int p_43333_, float p_43334_, float p_43335_, int p_43336_, Supplier<Ingredient> p_43337_) {
+        this.level = p_43332_;
+        this.uses = p_43333_;
+        this.speed = p_43334_;
+        this.damage = p_43335_;
+        this.enchantmentValue = p_43336_;
+        this.repairIngredient = new LazyLoadedValue<>(p_43337_);
+    }
+
+    public int getUses() {
+        return this.uses;
+    }
+
+    public float getSpeed() {
+        return this.speed;
+    }
+
+    public float getAttackDamageBonus() {
+        return this.damage;
+    }
+
+    public int getLevel() {
+        return this.level;
+    }
+
+    public int getEnchantmentValue() {
+        return this.enchantmentValue;
+    }
+
+    public Ingredient getRepairIngredient() {
+        return this.repairIngredient.get();
+    }
+
+    @javax.annotation.Nullable public net.minecraft.tags.Tag<net.minecraft.world.level.block.Block> getTag() {
+        return switch (this){
+            case PINK_DIAMOND -> BlockTags.NEEDS_DIAMOND_TOOL;
+        };
+    }
+}
